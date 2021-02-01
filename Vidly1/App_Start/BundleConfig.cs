55fc8a9299b280f850a975dffb2e8849d4615944
William Oliveira <1704639@aluno.univesp.br>
2021-01-31 21:48:33 -0300

    exercise

--- Vidly1/App_Start/BundleConfig.cs
+++ Vidly1/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace Vidly1
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css").Include(                      
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
--- Vidly1/Controllers/MoviesController.cs
+++ Vidly1/Controllers/MoviesController.cs
@@ -8,56 +8,25 @@ using Vidly1.ViewModels;
 
 namespace Vidly1.Controllers
 {
+    //Controller class
     public class MoviesController : Controller
     {
-        // GET:  Movies/Random
-        public ActionResult Random()
+        // GET: Movies/
+        public ActionResult Index()
         {
-            var movie = new Movie(){Name = "Shrek!"};
+            var movies = GetMovies();
 
-            var customers = new List<Customer>
+            return View(movies);
+
+        }
+
+        private object GetMovies()
+        {
+            return new List<Movie>
             {
-                new Customer {Name = "Customer 1 "},
-                new Customer {Name = "Customer 2 "},
-                new Customer {Name = "Customer 3 "},
-                new Customer {Name = "Customer 4 "},
-                new Customer {Name = "Customer 5 "},
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
             };
-
-            var viewModel = new RandomMovieViewModel 
-            { 
-                Customers = customers,
-                Movie = movie
-            };
-
-            return View(viewModel);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new {  page = 1, SortBy = "name"});
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id );
-        }
-
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("PageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year:regex(\\d{4}):min(0)}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-                return Content(year + "/" + month);
         }
     }
 }
