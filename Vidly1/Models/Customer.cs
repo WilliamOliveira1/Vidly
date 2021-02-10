3107ddb203a24f16d429fa3522f256b4b05f2a78
William Oliveira <1704639@aluno.univesp.br>
2021-02-09 21:52:28 -0300

    add membershipType

--- Vidly1/Models/Customer.cs
+++ Vidly1/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace Vidly1.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MenbershipTypeId { get; set; }
     }
 }
