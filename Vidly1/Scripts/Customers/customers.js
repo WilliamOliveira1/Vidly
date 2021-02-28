2280de809eae3eb9d5d917e5fd07d39e9225ec43
William Oliveira <1704639@aluno.univesp.br>
2021-02-28 20:28:29 -0300

    setup bootbox and using ajax and api to delete a customer

--- Vidly1/Scripts/Customers/customers.js
+++ Vidly1/Scripts/Customers/customers.js
@@ -1,5 +1,16 @@
-﻿if ($('#Customer_Name').attr('value') == "") {
-        $("#deleteCustomerButton").hide();
-} else {
-        $("#deleteCustomerButton").show();
-}
+﻿$(document).ready(function () {
+    $(".js-delete").on("click", function () {
+        var button = $(this);
+        bootbox.confirm("Are you sure you want to delete this customer?", function (result) {
+            if (result) {
+                $.ajax({
+                    url: "api/customers/" + button.attr("data-customer-id"),
+                    method: "DELETE",
+                    success: function () {
+                        button.parents("tr").remove();
+                    }
+                })
+            }
+        });
+    })
+})
