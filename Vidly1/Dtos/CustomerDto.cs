3244749c24a70273886a50d17ed2f18ccd523c61
William Oliveira <1704639@aluno.univesp.br>
2021-02-28 17:42:26 -0300

    set config to Json

--- Vidly1/Dtos/CustomerDto.cs
+++ Vidly1/Dtos/CustomerDto.cs
@@ -10,12 +10,11 @@ namespace Vidly1.Dtos
     public class CustomerDto
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Please enter customer's name")] 
+        
+        [Required]
         [StringLength(255)] // This is how we limit the string length
         public string Name { get; set; }
 
-        [Min18YearsIfAMember]
         public DateTime Birthday { get; set; }
 
         public bool IsSubscribedToNewsLetter { get; set; }
